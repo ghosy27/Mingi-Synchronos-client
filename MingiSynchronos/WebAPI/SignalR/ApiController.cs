3cf51b27e7152d806f37115d293497d2c080cac7
Ghosy <71652577+ghosy27@users.noreply.github.com>
2025-10-24 02:03:55 +0200

    Moved to a Subdomain

--- MingiSynchronos/WebAPI/SignalR/ApiController.cs
+++ MingiSynchronos/WebAPI/SignalR/ApiController.cs
@@ -22,7 +22,7 @@ namespace MingiSynchronos.WebAPI;
 public sealed partial class ApiController : DisposableMediatorSubscriberBase, IMingiHubClient
 {
     public const string MainServer = "Mingi-Server";
-    public const string MainServiceUri = "https://ghosy.site:6000";
+    public const string MainServiceUri = "https://fxiv.ghosy.site:6000";
 
     private readonly DalamudUtilService _dalamudUtil;
     private readonly HubFactory _hubFactory;
